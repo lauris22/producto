8ec708095d6a201f7901ef65af692db9b458408f
lauris22 <llauraalejandra11@gmail.com>
2024-08-13 16:02:39 -0500

    Update Datos.php

--- Datos.php
+++ Datos.php
@@ -1,27 +1,22 @@
  <?php
-        $nombrel="POST" ['nombrel'];
-        $preciol="POST" ['preciol'];
-        
-        $nombre2="_POST" ['nombre2'];
-        $precio2="_POST" ['precio2'];
-        
-        $nombre3="_POST" ['nombre3']; 
-        $precio3="_POST" ['precio3'];
-        
-        $suma=$preciol+$precio2+$precio3;
-        
-        $descuento=$suma*0.16/100;
-        
-        $total_pagar=$suma-$descuento;
-        
-        echo "productos:";
-        echo "$nombrel, $nombre2, $nombre3";
-        echo "<'/br'>";
+    $Producto1=$_POST['Producto1'];
+    $Precio1=$_POST['Precio1'];
+    $producto2=$_POST['Producto2'];
+    $Precio2=$_POST['Precio2'];
+    $producto3=$_POST['Producto3'];
+    $Precio3=$_POST['Precio3'];
+    $suma=$Precio1+$Precio2+$Precio3;
+    $descuento=$suma*0.16/100;
+    $total=$suma-$descuento;
+    
+       echo "productos:";
+        echo "$Producto1, $producto2, $producto3";
+        echo "</br>";
         echo "suma de precios: ".$suma;
-        echo "<'/br'>";
+        echo "</br>";
         echo "Descuento: ".$descuento;
-        echo "<'/br'>";
-        echo "total a pagar: ".$total_pagar;
+        echo "</br>";
+        echo "total a pagar: ".$total;
         ?>
 
 
